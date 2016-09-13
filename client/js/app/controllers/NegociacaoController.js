f5f24a5994655c379f77d2d95ce571c7a6f3ea23
Adriano Mota <adrianowsh@hotmail.com>
2016-09-12 23:39:07 -0300

    Eliminando código duplicado com arrow

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,7 +6,11 @@ class NegociacaoController{
        this._inputData = $('#data');
        this._inputQuantidade = $('#quantidade');
        this._inputValor = $('#valor');
-       this._listaNegociacoes = new ListaNegociacoes();
+       
+       this._listaNegociacoes = new ListaNegociacoes(model =>
+            this._negociacoesView.update(model));
+
+
        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
        this._negociacoesView.update(this._listaNegociacoes);
        this._mensagem = new Mensagem(); 
@@ -21,7 +25,14 @@ class NegociacaoController{
        this._mensagem.texto = "Negociação adicionada com sucesso!";
        this._mensagemView.update(this._mensagem);
        this._limpaFormulario();
-       this._negociacoesView.update(this._listaNegociacoes);
+    }
+
+    apagar(){
+
+        this._listaNegociacoes.apagar();
+
+        this._mensagem.texto = "Lista de Negociações apagadas com sucesso!";
+        this._mensagemView.update(this._mensagem);
     }
 
     _criaNegociacao(){
